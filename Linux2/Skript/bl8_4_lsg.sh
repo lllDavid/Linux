7bad4a499e3385ce8ac6aa494c43bff0a08656af
lllDavid <imperfectrising5527@gmail.com>
2024-07-09 16:08:00 +0200

    sync

--- Linux2/Skript/bl8_4_lsg.sh
+++ Linux2/Skript/bl8_4_lsg.sh
@@ -3,10 +3,9 @@
 # Skript zum Einrichten von temporären Accounts. Diese Accounts müssen (1 pro
 # Zeile) mit ihrem Usernamen in der Datei "accounts.txt" zu finden sein!
 
-while read s; do
-    pw=$s"-1234"
-    useradd $s
-    echo -e "$pw\n$pw" | passwd --stdin $s
-    chage -d 0 -E 2024-05-31 $s
-done < accounts.txt
-
+while read s; do                    # Schleife liest jede Zeile aus der Datei accounts.txt
+    pw=$s"-1234"                   # Erstellt das Passwort als Kombination aus Benutzername und "-1234"
+    useradd $s                      # Fügt den Benutzer hinzu (angenommen, der Benutzername ist in $s gespeichert)
+    echo -e "$pw\n$pw" | passwd --stdin $s  # Setzt das Passwort für den Benutzer auf das generierte Passwort
+    chage -d 0 -E 2024-05-31 $s     # Legt das Datum fest, an dem das Passwort geändert werden muss (erstes Login am 31. Mai 2024)
+done < accounts.txt                # Weiterlesen der nächsten Zeile aus accounts.txt
